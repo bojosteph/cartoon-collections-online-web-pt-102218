0da8bf655b35cd811bd9c4124e89b02c5a3e2fb4
Roberto Samia <bojosteph@gmail.com>
2018-10-27 16:31:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+def roll_call_dwarves(dwarves)
+     list = []
+     dwarves.each.with_index(1) do |character, index|
+     list << "#{index}. *#{character}"
+   end
+   puts list
+ end
+
+
 
 def summon_captain_planet# code an argument here
   # Your code here
