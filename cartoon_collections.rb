f36e84eacc9b2ed8f58bd98946708e0a2c6cfecb
Roberto Samia <bojosteph@gmail.com>
2018-10-27 16:46:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  if calls.length > 4 
-    return true
-  else 
-    false
+  if calls.length > 4? true : false
+    
+  
+    
    end  
 end
 
