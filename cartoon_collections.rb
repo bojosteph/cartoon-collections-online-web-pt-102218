c988407967c437e0090be41551b138ea53696b77
Roberto Samia <bojosteph@gmail.com>
2018-10-27 17:04:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.find cheese_types
+  ingredients
   cheese_types
   
  end
